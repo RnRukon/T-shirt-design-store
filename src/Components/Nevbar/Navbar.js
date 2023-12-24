7a07253a702a7ebc541daf83625906f5e551c7b1
RnRukon <rukon.js@gmail.com>
2023-12-24 16:31:13 +0600

    update

--- src/Components/Nevbar/Navbar.js
+++ src/Components/Nevbar/Navbar.js
@@ -41,40 +41,48 @@ const Navbar = () => {
 
 
     return (
-        <nav className=" bg-white shadow flex justify-between items-center px-20">
+        <nav className=" bg-white shadow flex justify-between items-center md:px-20 px-5">
             <div className="text-center text-gray-950 text-[64px] font-black font-['Montserrat']">PB</div>
 
-            <div className="flex items-center gap-5">
-                <ol className="flex items-center gap-5">
-                    {
-                        navLink?.map(data => {
-                            return (
-                                <li key={data?.path}>
-                                    <a href={data?.path}
-                                    className="text-gray-950 text-base font-bold font-['Montserrat'] rounded px-[15px] py-[12px] hover:bg-orange-500 hover:text-white transition hover:transition "
-                                    >{data?.title}</a>
-                                </li>
-                            )
-                        })
-                    }
-                </ol>
-                <ol className="flex items-center gap-2">
-                    {
-                        socialLink?.map(data => {
-                            return (
-                                <li key={data?.path}>
-                                    <a href={data?.link}>
-                                        <img
-                                            src={data?.icon}
-                                            height={30}
-                                            width={30}
-                                            alt={data?.title}/>
-                                    </a>
-                                </li>
-                            )
-                        })
-                    }
-                </ol>
+            <div className=" hidden lg:block">
+                <div className=" flex items-center gap-5">
+                    <ol className="flex items-center gap-5">
+                        {
+                            navLink?.map(data => {
+                                return (
+                                    <li key={data?.path}>
+                                        <a href={data?.path}
+                                           className="text-gray-950 text-base font-bold font-['Montserrat'] rounded px-[15px] py-[12px] hover:bg-orange-500 hover:text-white transition hover:transition "
+                                        >{data?.title}</a>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ol>
+                    <ol className="flex items-center gap-2">
+                        {
+                            socialLink?.map(data => {
+                                return (
+                                    <li key={data?.path}>
+                                        <a href={data?.link}>
+                                            <img
+                                                src={data?.icon}
+                                                height={30}
+                                                width={30}
+                                                alt={data?.title}/>
+                                        </a>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ol>
+                </div>
+            </div>
+
+            <div className="lg:hidden block">
+                <a href="/">
+                    <img src="/images/threeLine.png" alt=""/>
+                </a>
             </div>
         </nav>
     );
