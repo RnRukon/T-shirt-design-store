df46df7e41d982f335cd59500ffdec675e88820b
RnRukon <rukon.js@gmail.com>
2024-01-11 11:26:44 +0600

    chat button

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -7,12 +7,12 @@ const Chat = () => {
             <div className="relative ">
                 <div className="flex flex-col gap-1  pr-2">
                         <button
-                            className={`duration-1000 relative  ${show?"-top-[1px]":" top-32"}`}
+                            className={`duration-1000 relative  h-[60px] w-[60px] rounded-full ${show?"-top-[1px]":" top-32"}`}
                         >
                             <img className="h-[60px] " src="/images/messenger.png" alt="chat"/>
                         </button>
                         <button type="button"
-                        className={`duration-1000 relative   ${show?"-top-[1px] transition-all":" top-16"}`}
+                        className={`duration-1000 relative h-[60px] w-[60px] rounded-full ${show?"-top-[1px] transition-all":" top-16"}`}
                         >
                             <img className="h-[60px] " src="/images/whatsapp.png" alt="chat"/>
                         </button>
@@ -20,7 +20,7 @@ const Chat = () => {
                         <button
                             onClick={() => setShow(state => !state)}
                             type='button'
-                            className=" relative z-10 bg-[#f97316] rounded-full  "
+                            className=" relative z-10 h-[60px] w-[60px] bg-[#f97316] rounded-full  "
                         >
                             <img className="h-[60px] p-3 " src="/images/chat.png" alt="chat"/>
                         </button>
