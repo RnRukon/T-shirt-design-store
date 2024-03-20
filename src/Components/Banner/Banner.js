20e74f592314d31fc0283c2d2881fea511fca6b0
rukonpro <rukon.js@gmail.com>
2024-03-20 19:53:17 +0600

    update banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Marquee from "react-fast-marquee";
 
 const Banner = () => {
+    const banners=[1,2,3,4,5,6,7]
     return (
         <div  className="pt-[95px]">
-            <img  className="w-full max-h-[70vh] object-fill" src="/images/banner.avif" alt=""/>
+            <Marquee>
+                {
+                    banners.map(banner=>{
+                        return (
+                            <img className="max-h-[30vh] md:max-h-[40vh] lg:max-h-[50vh] xl:max-h-[60vh]" src="/images/banner.jpg" alt=""/>
+                        )
+                    })
+                }
+
+            </Marquee>
         </div>
     );
 };
