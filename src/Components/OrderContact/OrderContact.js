d82ae61512c89ad031062d972611cf996a318c75
rukonpro <rukon.js@gmail.com>
2024-03-29 23:35:13 +0600

    update banner ,products and order now button

--- src/Components/OrderContact/OrderContact.js
+++ src/Components/OrderContact/OrderContact.js
@@ -17,17 +17,31 @@ const OrderContact = ({isOpen,closeModal}) => {
                         </button>
                     </div>
                     <h1 className="text-2xl font-bold mb-4 uppercase text-center">Contact us before Ordering</h1>
-                    <div className="max-w-lg mx-auto text-center">
+                    <div className="max-w-lg mx-auto text-center pb-10">
                         <p className="pt-5">Thanks for choosing out package! We're exited to work with you. Before you order, let's chate
                             about your needs.Please contact us to discuss.Thanks!</p>
 
                         <div className="flex gap-4 justify-center mt-16">
-                            <button type="button" className="bg-orange-700 text-white font-bold text-lg rounded-full px-5 py-3">
-                                WHATSAPP
-                            </button>
-                            <button type="button" className="bg-orange-700 text-white font-bold text-lg  rounded-full px-5 py-3">
-                                MESSENGER
-                            </button>
+                            <a
+                                className="hover:bg-[#f97316] hover:text-white border-2 border-gray-700 hover:border-[#f97316] duration-100 font-bold text-lg rounded-full px-5 py-3"
+                                href="https://api.whatsapp.com/send?phone=1323169833"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <button type="button"
+                                       >
+                                    WHATSAPP
+                                </button>
+                            </a>
+                            <a href="https://www.facebook.com/messages/t/118442278025815"
+                               target="_blank"
+                               rel="noreferrer"
+                               className="hover:bg-[#f97316] hover:text-white border-2 border-gray-700 hover:border-[#f97316] duration-100 font-bold text-lg rounded-full px-5 py-3"
+                            >
+                                <button type="button">
+                                    MESSENGER
+                                </button>
+                            </a>
                         </div>
                     </div>
                 </div>
